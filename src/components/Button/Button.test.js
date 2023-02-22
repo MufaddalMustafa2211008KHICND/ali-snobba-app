5670eb39445832df8686a9661e9dccd8c9a131ac
Mufaddal Mustafa <mufaddal.mustafa@xloopdigital.com>
2023-02-22 12:21:17 +0500

    first failing test for button component

---
+++ src/components/Button/Button.test.js
@@ -0,0 +1,13 @@
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import {Button} from './Button';
+
+test('button renders', async () => {
+    //Arrange
+    render(<Button>I'm a button</Button>)
+    //Act
+    const button = await screen.findByText("I'm a button");
+    //Assert
+    expect(button).toBeInTheDocument();
+});
